9d4d4a9f14c90c5a55e77a9ba4265ab8c2ac82bf
William Chavarria <wchavarria@tigo.com.gt>
2017-09-02 23:48:54 -0600

    primera version funcional

--- arima.R
+++ arima.R
@@ -4,106 +4,131 @@
   # opcion de visualizacion de estructura
   options(str = strOptions(vec.len = 2))
 
+  # definir local
+  Sys.setlocale("LC_TIME", "C")
+
   # cargar funciones
   source('instalar_paquetes.R')
 
   # definir paquetes a utilizar
   pkgs <- c(
     'tidyquant', 'dplyr', 'broom', 'ggplot2', 'purr',
-    'stringr', 'data.table', 'timetk'
+    'stringr', 'data.table', 'timetk', 'cranlogs',
+    'timekit', 'sweep', 'forecast', 'geofacet', 'magrittr'
     )
 
+  # instalar paquetes necesarios y cargar librerias
   ipak(pkgs)
-  
-  
+
 #   ____________________________________________________________________________
 #   LEER DATASET                                                            ####
 
-  # definir columnas a conservar
-  colsToKeep <- c('fecha_uso_material',
-                  'tipo_ticket',
-                  'codigo_ebs',
-                  'detalle',
-                  'cantidad')
+  # read dataset
+  osp <- fread(
+          input      = 'osp_materials.csv',
+          data.table = FALSE
+         ) %>% as.tibble()
+  
+  
+  # borrar objetos innecesarios
+  rm(list = setdiff(ls(), 'osp'))
 
-  # leer dataset
-  ds <- fread(
-          input      = 'materiales.csv',
-          data.table = FALSE,
-          select     = colsToKeep, 
-          col.names  = c('fecha',
-                         'area',
-                         'codigo',
-                         'descripcion',
-                         'cantidad')
-        )
+##  ............................................................................
+##  AGREGACION MENSUAL                                                      ####
 
-#   ____________________________________________________________________________
-#   DATA CARPENTRY                                                          ####
-
+  # agregacion mensual
+  osp.mensual <- osp %>%
+    mutate_at('fecha', dmy) %>%
+    group_by(material) %>%
+    tq_transmute(
+      select     = cantidad,
+      mutate_fun = apply.monthly,
+      FUN        = sum,
+      na.rm      = TRUE,
+      col_rename = 'total'
+      
+    ) %>% select(fecha, material, total)
   
 ##  ............................................................................
-##  CODIGOS VALIDOS PARA FILTRAR                                            ####
+##  VISUALIZACION MENSUAL                                                   ####
 
-  
-  # codigos validos
-  codigos <- c('OSP0000111',
-               'TE0762',
-               'OSP0000193',
-               'TE0993',
-               'OSP0000185',
-               'TE0991',
-               'OSP0000167')
-  
-  categor <- c('FIBRA OPTICA DE 48 HILOS',
-               'FIBRA OPTICA DE 48 HILOS',
-               'MUFAS PUP',
-               'MUFAS PUP',
-               'BANDEJAS',
-               'BANDEJAS',
-               'CRUCETAS')
-  
-  # crear tibble
-  mat <- tibble(codigo   = codigos,
-                material = categor)
-  
-  # pre-definir orden de columnas
-  ordenCols <- c('fecha', 'material', 'cantidad')
-  
-##  ............................................................................
-##  TRANSFORMAR DATASET                                                     ####
-
-  ds.1 <- ds %>%
-          mutate_at('fecha',ymd, tz = 'America/Guatemala') %>%
-          filter(codigo %in% mat$codigo) %>%
-          left_join(mat, by = 'codigo') %>%
-          mutate_if(is.character, toupper) %>%
-          as.tibble %>%
-          select(ordenCols) %>%
-          arrange(fecha) 
-  
-  
-##  ............................................................................
-##  VISUALIZACION POR DIA                                                   ####
-
-  # visualizar el uso de materiales
-  ds.1 %>%
-    ggplot(aes(x = fecha, y = cantidad, color = material)) +
+  # visualizar el uso de materiales de forma mensual
+  osp.mensual %>%
+    ggplot(aes(x = fecha, y = total, color = material)) +
     # Data
-    geom_point(alpha = 0.5) +
+    geom_point() +
+    geom_smooth(method = 'loess') +
     facet_wrap(~ material, ncol = 2, scale = "free_y") +
     # Aesthetics
-    labs(title = "Consumo de materiales: Uso Mensual", x = "",
-         subtitle = "2017-01-01 through 2017-12-31",
+    labs(title = "Consumo promedio de materiales por mes", x = "",
+         subtitle = "Enero-2015 a Agosto-2017", y = 'Media de Consumo Mensual',
          caption = "Departamento de Tx Support") +
+    expand_limits(y = 0) +
     scale_color_tq() +
     theme_tq() +
     theme(legend.position = "none")
+  
+  
+  
+#   ____________________________________________________________________________
+#   MATERIALS FORECASTING                                                   ####
 
+  
+  # ajuste y pronostico
+  osp.mensual %<>% 
+    nest() %>%                             
+    mutate(
+      data_ts = map(.x     = data, 
+                    .f     = tk_ts, 
+                    freq   = 12,
+                    start  = 2015,
+                    silent = TRUE)
+      ) %>%
+    mutate(fit = map(.x = data_ts,
+                     .f = auto.arima)
+           ) %>%
+    mutate(pronostico = map(.x = fit,
+                            .f = forecast,
+                            h = 12))
+  
+  # aplicar sweep
+  osp.sweep <- osp.mensual %>%
+    mutate(sweep = map(.x           = pronostico,
+                       .f           = sw_sweep,
+                       timetk_idx  = TRUE,
+                       rename_index = 'date')
+           ) %>%
+    select(material, sweep) %>%
+    unnest()
+  
+  
+#   ____________________________________________________________________________
+#   GRAFICAR PRONOSTICOS                                                    ####
 
-##  ............................................................................
-##  VISUALIZACION DEL CONSUMO POR MES                                       ####
+  osp.sweep %>%
+    ggplot(aes(x = date, y = total, color = key)) +
+    # Prediction intervals
+    geom_ribbon(aes(ymin = lo.95, ymax = hi.95), 
+                fill = "#D5DBFF", color = NA, size = 0) +
+    geom_ribbon(aes(ymin = lo.80, ymax = hi.80, fill = key), 
+                fill = "#596DD5", color = NA, size = 0, alpha = 0.8) +
+    # Actual & Forecast
+    geom_point() +
+    geom_line(size = 0.8) +
+    facet_wrap(~ material, ncol = 2, scale = "free") +
+    theme_tq() +
+    scale_color_tq(theme = 'dark') +
+    ggtitle("Outside Plant (OSP) Materials, 12-Months Forecast") +
+    xlab("") +
+    ylab("Cantidad")
 
+ 
+  # imprimir la precision del modelo
+  osp.mensual %>% 
+    mutate(glance = map(.x = fit,
+                        .f = sw_glance)
+    ) %>% unnest(glance, .drop = TRUE)
+  
   
   
   
